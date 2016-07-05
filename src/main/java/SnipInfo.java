6d3c68fea98aa9e095b3aaab7cef655ba82d873f
vladimir <motoc_vladimir@mail.ru>
2016-07-05 17:28:31 +0300

    fix

--- src/main/java/SnipInfo.java
+++ src/main/java/SnipInfo.java
@@ -2,14 +2,15 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.regex.Pattern;
 import java.util.regex.*;
-public class Harness {
+public class SnipInfo {
 
 
     public static void main(String[] args) {
         String input= "Del Mar Photonics, Inc. 4119 Twilight Ridge San Diego, CA 92130 tel: (858) 876-3133 fax: (858) 630-2376 Skype: delmarphotonics e-mail: ruksineanu@gmail.com delmar@delmar.ru";
         Pattern e_mail = Pattern.compile("\\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b", Pattern.CASE_INSENSITIVE);
-        Pattern tel = Pattern.compile("([01][- .])?(\\(\\d{3}\\)|\\d{3})[- .]?\\d{3}[- .]\\d{41}", Pattern.CASE_INSENSITIVE);
-        Pattern fax = Pattern.compile("\\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b", Pattern.CASE_INSENSITIVE);
+        Pattern tel = Pattern.compile("tel:\\(*\\+*[1-9]{0,3}\\)*-*[1-9]{0,3}[-. /]*\\(*[2-9]\\d{2}\\)*[-. /]*\\d{3}[-. /]*\\d{4} *e*x*t*\\.* *\\d{0,4}", Pattern.CASE_INSENSITIVE);
+        Pattern fax = Pattern.compile("fax:\\(*\\+*[1-9]{0,3}\\)*-*[1-9]{0,3}[-. /]*\\(*[2-9]\\d{2}\\)*[-. /]*\\d{3}[-. /]*\\d{4} *e*x*t*\\.* *\\d{0,4}", Pattern.CASE_INSENSITIVE);
+        Pattern skype = Pattern.compile("[S|s]kype:[\\s]{1,5}[a-z0-9]{1,15}", Pattern.CASE_INSENSITIVE);
         Matcher matcher = e_mail.matcher(input);
         Set<String> emails = new HashSet<>();
         while(matcher.find()) {
@@ -30,6 +31,16 @@ public class Harness {
            String ad = matcher.group();
            System.out.println(ad);
        }
+        matcher = fax.matcher(input);
+        while (matcher.find()) {
+            String ad = matcher.group();
+            System.out.println(ad);
+        }
+        matcher = skype.matcher(input);
+        while (matcher.find()) {
+            String ad = matcher.group();
+            System.out.println(ad);
+        }
 
     }
 }
--- src/main/java/adressexecutor/AdressExecutor.java
+++ src/main/java/adressexecutor/AdressExecutor.java
@@ -9,9 +9,7 @@ import opennlp.tools.util.StringList;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -31,9 +29,10 @@ public class AdressExecutor {
         textTokens.add("city of san diego official website the city of san diego's official website offers information and online services for departments, business assistance, job opportunities, attractions, beaches, community services, and more. service line warranty program launches, offered by slwa, the preferred service line warranty partner of the city of san diego' visit the slwa website at www.slwofa.com or call 1-855-261-9830 (toll free) to sign up or get more details. to learn about the partnership with the city, please visit the slwa page.");
         textTokens.add("(800) 682-9680 · 1945 Gardena Ave Suite 100. Glendale, CA 91204 · Web Design, Marketing ... trendsetter! You could be the first review for USA Link System.");
         textTokens.add("5500 Campanile Drive San Diego, CA 92182 Tel: 619-594-5200 Copyright 2016");
-
-        Pattern adressREGEX = Pattern.compile("([\\d]{1,4})($)", Pattern.CASE_INSENSITIVE);
+        textTokens.add("USO San Diego - Neil Ash Airport Center. 0 Reviews. 3707 North Harbor Dr, Terminal 1, Lindbergh Field, San Diego, CA 92101; Add Photo. Add Review. Get Directions. Phone");
+        Pattern numREGEX = Pattern.compile("([\\d]{1,4})($)", Pattern.CASE_INSENSITIVE);
         Pattern filterREGEX = Pattern.compile("[^\\w\\s\\d]|(Tel)", Pattern.CASE_INSENSITIVE);
+        Pattern addressREGEX = Pattern.compile("([0-9]{4})([^0-9]{4}.*)([A-Z]{2}\\s{1,5}[0-9]{5})", Pattern.CASE_INSENSITIVE);
         // Load the model file downloaded from OpenNLP
         // http://opennlp.sourceforge.net/models-1.5/
         TokenNameFinderModel loc = new TokenNameFinderModel(new File("en-ner-location.bin"));
@@ -42,9 +41,13 @@ public class AdressExecutor {
 
         Tokenizer tokenizer = SimpleTokenizer.INSTANCE;
         List<String> results = new ArrayList<>();
-
+        List<String> regResult = new ArrayList<>();
         for (int i = 0; i < textTokens.size(); i++) {
             String sentence = textTokens.get(i);
+            Matcher regAddress = addressREGEX.matcher(sentence);
+            while (regAddress.find()){
+                regResult.add(regAddress.group());
+            }
             // Split the sentence into tokens
             String[] tokens = tokenizer.tokenize(sentence);
             // Find the names in the tokens and return Span objects
@@ -70,7 +73,7 @@ public class AdressExecutor {
                 for (String fLoc : filtrLocation) {
                     if (fLoc != null) {
                         String sent = sentence.substring(0, sentence.indexOf(fLoc));
-                        Matcher adressMatcher = adressREGEX.matcher(sent.trim());
+                        Matcher adressMatcher = numREGEX.matcher(sent.trim());
                         if (adressMatcher.find()) {
                             address = adressMatcher.group();
                             address += " " + fLoc;
@@ -84,6 +87,12 @@ public class AdressExecutor {
 
             }
         }
-        System.out.println("Найденные адресса: \n" + "-------------------" + results.toString());
-    }
+        Collections.replaceAll(regResult, ",", "");
+        for (String result : results) {
+            System.out.println("OpenNLP:        " + result);
+
+        }
+        for (String s : regResult) {
+            System.out.println("REGEX:          "+ s);
+        }    }
     }
