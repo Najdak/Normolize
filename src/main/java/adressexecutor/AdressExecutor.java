55c8ca5185e7d043e3f519f99405ee818b71fec3
vladimir <motoc_vladimir@mail.ru>
2016-07-05 12:20:52 +0300

    ferst

--- src/main/java/adressexecutor/AdressExecutor.java
+++ src/main/java/adressexecutor/AdressExecutor.java
@@ -23,19 +23,20 @@ public class AdressExecutor {
     public static List<String> textTokens = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
-        textTokens.add("del mar photonics, inc. 4119 Twilight Ridge San Diego, CA 92130 tel: (858) 876-3133 fax: (858) 630-2376 skype: delmarphotonics sales@dmphotonics.com");
-        textTokens.add("del mar photonics, inc. product portfolio includes ultrafast laser oscillators and amplifiers based on ti:sapphire, cr:forsterite, er- and yb- doped fibers; ... ");
-        textTokens.add("del mar photonics 4119 twilight ridge | san diego, ca 92130 usa | tel::(858) 876-3133 fax::(858) 630-2376 e-mail::optics@dmphotonics.com url::www.dmphotonics.com ");
+        textTokens.add("Del Mar Photonics, inc. 4119 Twilight Ridge San Diego, CA 92130 tel: (858) 876-3133 fax: (858) 630-2376 skype: delmarphotonics sales@dmphotonics.com");
+        textTokens.add("Del Mar Photonics, inc. product portfolio includes ultrafast laser oscillators and amplifiers based on ti:sapphire, cr:forsterite, er- and yb- doped fibers; ... ");
+        textTokens.add("Del Mar Photonics 4119 Twilight Ridge | San Diego, ca 92130 usa | tel::(858) 876-3133 fax::(858) 630-2376 e-mail::optics@dmphotonics.com url::www.dmphotonics.com ");
         textTokens.add("Del Mar Photonics, Inc. product portfolio includes ultrafast laser oscillators and amplifiers based on Ti:Sapphire, Cr:Forsterite, Er- and Yb- doped fibers;");
         textTokens.add("Learn about working at Del Mar Photonics. Join LinkedIn today for free. See who you know at Del Mar Photonics, leverage your professional network, ");
         textTokens.add("city of san diego official website the city of san diego's official website offers information and online services for departments, business assistance, job opportunities, attractions, beaches, community services, and more. service line warranty program launches, offered by slwa, the preferred service line warranty partner of the city of san diego' visit the slwa website at www.slwofa.com or call 1-855-261-9830 (toll free) to sign up or get more details. to learn about the partnership with the city, please visit the slwa page.");
         textTokens.add("(800) 682-9680 · 1945 Gardena Ave Suite 100. Glendale, CA 91204 · Web Design, Marketing ... trendsetter! You could be the first review for USA Link System.");
-        
-        Pattern adressREGEX = Pattern.compile("\\d{2,4}$");
-        Pattern filterREGEX = Pattern.compile("[^\\w\\s\\d]");
+        textTokens.add("5500 Campanile Drive San Diego, CA 92182 Tel: 619-594-5200 Copyright 2016");
+
+        Pattern adressREGEX = Pattern.compile("([\\d]{1,4})($)");
+        Pattern filterREGEX = Pattern.compile("[^\\w\\s\\d]|([T|t]el)");
         // Load the model file downloaded from OpenNLP
         // http://opennlp.sourceforge.net/models-1.5/
-        TokenNameFinderModel loc = new TokenNameFinderModel(new File("/home/vladimir/Projects/izsearch/src/main/resources/data/en-ner-location.bin"));
+        TokenNameFinderModel loc = new TokenNameFinderModel(new File("en-ner-location.bin"));
         // Create a NameFinder using the model
         NameFinderME locF = new NameFinderME(loc);
 
@@ -48,6 +49,8 @@ public class AdressExecutor {
             String[] tokens = tokenizer.tokenize(sentence);
             // Find the names in the tokens and return Span objects
             Span[] locations = locF.find(tokens);
+         //   System.out.println(Arrays.toString(Span.spansToStrings(locations, tokens)));
+
             List<String> location = Arrays.asList(Span.spansToStrings(locations, tokens));
 
             if (locations != null && locations.length > 1) {
@@ -63,16 +66,16 @@ public class AdressExecutor {
                         }
                     }
                 }
-                String adress = "";
+                String address = "";
                 for (String fLoc : filtrLocation) {
-                    if (fLoc != null && fLoc.length() > 3) {
-                        String sent = sentence.substring(0, sentence.indexOf(fLoc)).trim();
-                        Matcher adressMatcher = adressREGEX.matcher(sent);
+                    if (fLoc != null) {
+                        String sent = sentence.substring(0, sentence.indexOf(fLoc));
+                        Matcher adressMatcher = adressREGEX.matcher(sent.trim());
                         if (adressMatcher.find()) {
-                            adress = adressMatcher.group();
-                            adress += " " + fLoc;
-                            adress = adress.replaceAll("[(\\[)|(\\])|,]", "");
-                            results.add(adress);
+                            address = adressMatcher.group();
+                            address += " " + fLoc;
+                            address = address.replaceAll("[(\\[)|(\\])|,]", "");
+                            results.add(address.trim());
 
                         }
                     }
@@ -81,5 +84,6 @@ public class AdressExecutor {
 
             }
         }
+        System.out.println("Найденные адресса: \n" + "-------------------" + results.toString());
     }
     }
