194bd41244d1b1589d30d75ccf11d2c356785df9
vladimir <motoc_vladimir@mail.ru>
2016-08-25 12:55:39 +0300

    piu piu piuuuuuuu

--- src/main/java/CSVebatiVBazu.java
+++ src/main/java/CSVebatiVBazu.java
@@ -1,3 +1,5 @@
+import org.apache.log4j.Logger;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -13,11 +15,11 @@ import java.util.List;
  * Created by vladimir on 16.08.16.
  */
 public class CSVebatiVBazu {
-
 	public static String user = "postgres";
 	public static String password = "Aqpl308E";
 	public static String url = "jdbc:postgresql://70.167.8.45:5433/faves_db_new_2?characterEncoding=utf8";
 	public static Connection connection;
+	public static String EXCEL_PATH = "/home/vladimir/Downloads/Names-_1_.csv";
 	static {
 		try {
 			DriverManager.registerDriver(new org.postgresql.Driver());
@@ -32,14 +34,9 @@ public class CSVebatiVBazu {
 	public static void main(String[] args) throws SQLException {
 		List<String[]> excel = getFromExcel();
 
-		DriverManager.registerDriver(new org.postgresql.Driver());
-		Connection connection = DriverManager.getConnection(url, user, password);
-		connection.setAutoCommit(false);
-
-
 		for (String[] strings : excel) {
 			if (strings[4].equals("1")){
-				Update(strings[1], new Integer(strings[0]));
+				UpdateTitle(strings[1], new Integer(strings[0]));
 			}else if (strings[4].equals("0")){
 				Delete(new Integer(strings[0]));
 			}
@@ -47,6 +44,23 @@ public class CSVebatiVBazu {
 
 		connection.close();
 
+	}
+
+	public static void UpdateTitle(String service_name, Integer id) throws SQLException {
+		PreparedStatement stm2 = connection.prepareStatement("SELECT * FROM bangs.bangs WHERE id = ?");
+		stm2.setInt(1, id);
+		ResultSet rs = stm2.executeQuery();
+		rs.next();
+		String upsql = "UPDATE bangs.bangs SET service_name = ? where id = ?";
+		PreparedStatement stm = connection.prepareStatement(upsql);
+		stm.setString(1, service_name);
+		stm.setInt(2, id);
+		stm.executeUpdate();
+		connection.commit();
+		stm.close();
+		System.out.println("Строка c ID: "+ id + " service_name изменен c [" + rs.getString("service_name")+"] на ["+service_name+"]" );
+		stm2.close();
+		rs.close();
 	}
 
 	public static void Update(String bang_url, Integer id) throws SQLException {
@@ -63,10 +77,15 @@ public class CSVebatiVBazu {
 		String delsql = "DELETE from bangs.bangs WHERE id = ?";
 		PreparedStatement stm = connection.prepareStatement(delsql);
 		stm.setInt(1, id);
-		stm.execute();
+		boolean chek = stm.execute();
 		connection.commit();
 		stm.close();
-		System.out.println(id + "DELETED");
+		if (chek){
+		System.out.println("Строка c ID: "+ id + " НЕ УДАЛОСЬ УДАЛИТЬ" );
+		}else {
+			System.out.println("Строка c ID: "+ id + " УДАЛЕНА" );
+		}
+
 	}
 	public static List<String> readRows(String fileName) {
 		List<String> rows = new ArrayList<>();
@@ -79,7 +98,7 @@ public class CSVebatiVBazu {
 	}
 
 	public static List<String[]> getFromExcel(){
-		List<String> lines = readRows("/home/vladimir/NewBangsEXTREME_EDITION_1.csv");
+		List<String> lines = readRows(EXCEL_PATH);
 		List<String[]> excel = new ArrayList<>();
 		for (String line : lines) {
 			String[] s = line.split(",");
