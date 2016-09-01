ce382a129e31e80596b8a94e70356d85a59e0f17
vladimir <motoc_vladimir@mail.ru>
2016-09-01 18:23:45 +0300

    geo index

--- src/main/java/CSVebatiVBazu.java
+++ src/main/java/CSVebatiVBazu.java
@@ -9,6 +9,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -19,7 +20,7 @@ public class CSVebatiVBazu {
 	public static String password = "Aqpl308E";
 	public static String url = "jdbc:postgresql://70.167.8.45:5433/faves_db_new_2?characterEncoding=utf8";
 	public static Connection connection;
-	public static String EXCEL_PATH = "/home/vladimir/Downloads/Names-_1_.csv";
+	public static String EXCEL_PATH = "/home/vladimir/Downloads/Names-_2_.csv";
 	static {
 		try {
 			DriverManager.registerDriver(new org.postgresql.Driver());
@@ -34,31 +35,43 @@ public class CSVebatiVBazu {
 	public static void main(String[] args) throws SQLException {
 		List<String[]> excel = getFromExcel();
 
+
 		for (String[] strings : excel) {
-			if (strings[4].equals("1")){
-				UpdateTitle(strings[1], new Integer(strings[0]));
-			}else if (strings[4].equals("0")){
+			if (strings[4].equals("0")){
 				Delete(new Integer(strings[0]));
 			}
+			if (strings[4].equals("1") || strings[4].isEmpty()){
+				UpdateTitle(strings[1], strings[2], new Integer(strings[0]));
+			}
 		}
+
+/*		List<Integer> l = new ArrayList<>(Arrays.asList(1266,1043,919,693,1109));
+		for (Integer integer : l) {
+			UpdateParentID(1266, integer);
+		}*/
 
 		connection.close();
 
 	}
 
-	public static void UpdateTitle(String service_name, Integer id) throws SQLException {
+	public static void UpdateTitle(String service_name, String bang_url, Integer id) throws SQLException {
 		PreparedStatement stm2 = connection.prepareStatement("SELECT * FROM bangs.bangs WHERE id = ?");
 		stm2.setInt(1, id);
 		ResultSet rs = stm2.executeQuery();
-		rs.next();
-		String upsql = "UPDATE bangs.bangs SET service_name = ? where id = ?";
+		String upsql = "UPDATE bangs.bangs SET service_name = ?, bang_url = ? where id = ?";
 		PreparedStatement stm = connection.prepareStatement(upsql);
 		stm.setString(1, service_name);
-		stm.setInt(2, id);
+		stm.setString(2, bang_url);
+		stm.setInt(3, id);
 		stm.executeUpdate();
 		connection.commit();
 		stm.close();
-		System.out.println("Строка c ID: "+ id + " service_name изменен c [" + rs.getString("service_name")+"] на ["+service_name+"]" );
+		System.out.println("Строка c ID: "+ id + " service_name и bang_url изменен c :"  );
+		while (rs.next()){
+			System.out.println(rs.getString("service_name"));
+			System.out.println(rs.getString("bang_url"));
+		}
+		System.out.println("На : " + service_name + " и " + bang_url);
 		stm2.close();
 		rs.close();
 	}
@@ -108,5 +121,21 @@ public class CSVebatiVBazu {
 			excel.add(s);
 		}
 		return excel;
+	}
+	public static void UpdateParentID(Integer parenthID, Integer id) throws SQLException {
+/*		PreparedStatement stm2 = connection.prepareStatement("SELECT * FROM public.categories WHERE id = ?");
+		stm2.setInt(1, id);
+		ResultSet rs = stm2.executeQuery();
+		rs.next();*/
+		String upsql = "UPDATE public.faves SET parent_id = ? where parent_id = ?";
+		PreparedStatement stm = connection.prepareStatement(upsql);
+		stm.setInt(1, parenthID);
+		stm.setInt(2, id);
+		stm.executeUpdate();
+		connection.commit();
+		stm.close();
+/*		System.out.println("Строка c ID: "+ id + " service_name изменен c [" + rs.getString("parent_id")+"] на ["+parenthID+"]" );
+		stm2.close();
+		rs.close();*/
 	}
 }
